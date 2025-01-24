a7c27c788eaa3faaa93b218f8701cb62ccfb920a
♔•アરเέຮ┼•♔ ぶ <streetdemonic003@gmail.com>
2025-01-23 23:08:38 -0300

    Update info.py

--- info.py
+++ info.py
@@ -4,15 +4,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = environ.get("API_ID" , "2099007")
-API_HASH = environ.get("API_HASH" , "b1b3d7b3b1b3d7b3b1b3d7b3b1b3d7b3")
-BOT_TOKEN = environ.get("BOT_TOKEN" , "2099007:AAH1b3d7b3b1b3d7b3b1b3d7b3b1b3d7b3")
-ADMIN = int(environ.get("ADMIN" , "1847899007"))
-CHAT_GROUP = int(environ.get("CHAT_GROUP", "-100"))
-LOG_CHANNEL = environ.get("LOG_CHANNEL", "-100")
-MONGO_URL = environ.get("MONGO_URL" , "mongodb://localhost:27017")
+API_ID = environ.get("API_ID" , "4954361")
+API_HASH = environ.get("API_HASH" , "43a786a8548a30f9d6887e36d53c0e64")
+BOT_TOKEN = environ.get("BOT_TOKEN" , "6390154239:AAG1raMRKyIyeZNh8q0BIb8FJVn1t5HiExU")
+ADMIN = int(environ.get("ADMIN" , "737737727"))
+CHAT_GROUP = int(environ.get("CHAT_GROUP", "-1001745424158"))
+LOG_CHANNEL = environ.get("LOG_CHANNEL", "-1001509201180")
+MONGO_URL = environ.get("MONGO_URL" , "mongodb+srv://thepanda:thepanda@cluster0.tumon.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 AUTH_CHANNEL = int(
-    environ.get("AUTH_CHANNEL", "-1001734958816")
+    environ.get("AUTH_CHANNEL", "-1001509201180")
 )
 FSUB = environ.get("FSUB", True)
 STICKERS_IDS = (
